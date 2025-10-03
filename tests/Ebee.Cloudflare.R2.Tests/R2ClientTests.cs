260b85fbe15ac4bd3013dd41bc901b0898f8ff2e
Ibrahim Suleiman <ebeeraheem@gmail.com>
2025-10-03 13:41:29 +0100

    implement tests for r2 client

--- tests/Ebee.Cloudflare.R2.Tests/R2ClientTests.cs
+++ tests/Ebee.Cloudflare.R2.Tests/R2ClientTests.cs
@@ -1,10 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Ebee.Cloudflare.R2.Buckets;
+using FluentAssertions;
+using Moq;
 
 namespace Ebee.Cloudflare.R2.Tests;
-internal class R2ClientTests
+
+/// <summary>
+/// Unit tests for <see cref="R2Client"/>.
+/// </summary>
+public class R2ClientTests
 {
+    [Fact]
+    public void Constructor_WithValidBucketsClient_ShouldSetBucketsProperty()
+    {
+        // Arrange
+        var mockBucketsClient = new Mock<IBucketsClient>();
+
+        // Act
+        var client = new R2Client(mockBucketsClient.Object);
+
+        // Assert
+        client.Buckets.Should().Be(mockBucketsClient.Object);
+    }
+
+    [Fact]
+    public void Constructor_WithNullBucketsClient_ShouldThrowArgumentNullException()
+    {
+        // Act
+        var act = () => new R2Client(null!);
+
+        // Assert
+        act.Should().Throw<ArgumentNullException>();
+    }
 }

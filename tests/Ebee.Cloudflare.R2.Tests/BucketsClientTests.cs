a77c112dfd111f8ce63a9f0104adfea9641e233c
Ibrahim Suleiman <ebeeraheem@gmail.com>
2025-10-03 13:32:55 +0100

    add moq and fluentassertions libraries; updated all packages; add r2 and bucket client test classes

---
+++ tests/Ebee.Cloudflare.R2.Tests/BucketsClientTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ebee.Cloudflare.R2.Tests;
+internal class BucketsClientTests
+{
+}
